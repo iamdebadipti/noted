1a5082cf6e775fb219331dd0ba4c0a8c9e579643
iamdebadipti <iamdebadipti@gmail.com>
2020-04-17 10:04:35 +0530

    fonts imported successfully

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+// stylesheets
+import './assets/icons/style.css';
+import './styles/main.scss';
 
 function App() {
-  return <h1>hello react</h1>;
+  return (
+    <div className="app">
+      <h1>fonts imported</h1>
+    </div>
+  );
 }
 
 export default App;
