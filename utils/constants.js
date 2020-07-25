2a05b25ee517dd501cd8d3ac19e689823cf72c6e
iamdebadipti <iamdebadipti@gmail.com>
2020-07-25 07:54:49 +0530

    added fake data & utils

---
+++ utils/constants.js
@@ -0,0 +1,17 @@
+export const NOTE_LONG_ACTION = [
+  {
+    title: 'Delete',
+    icon: 'trash',
+    action: 'DELETE'
+  },
+  {
+    title: 'Share',
+    icon: 'share',
+    action: 'SHARE'
+  },
+  {
+    title: 'Info',
+    icon: 'info',
+    action: 'INFO'
+  }
+];
