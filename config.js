7a6dd357703788ee5f189ccd255900300af43bf4
iamdebadipti <iamdebadipti@gmail.com>
2020-07-25 07:50:12 +0530

    added config file

---
+++ config.js
@@ -0,0 +1,12 @@
+// app theme
+export const theme = {
+  mainColor: '#4682B4',
+  textMain: '#444444',
+  textSecondary: '#777777',
+  lightGray: '#dedede',
+  headingFontSize: 20,
+  // common components
+  buttonStyle: {
+    backgroundColor: '#4682B4'
+  }
+};
