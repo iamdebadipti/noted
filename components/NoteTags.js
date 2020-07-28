f7c7db92965d1b2c169f6fc690b473d34d2ea50b
iamdebadipti <iamdebadipti@gmail.com>
2020-07-28 19:38:44 +0530

    input flex fixes & added remove, submit, onchange methods

--- components/NoteTags.js
+++ components/NoteTags.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { theme } from '../config';
 
 const NoteTags = ({ tagList }) => {
+  const [tagText, setTagText] = useState('');
+
+  // tag input text change handle -- setting the state
+  const handleTagInputChange = (text) => {
+    setTagText(text);
+  };
+
+  // keyboard submit function in tag input
+  const handleTagSubmit = async () => {
+    await console.warn(tagText);
+    await setTagText('');
+  };
+
+  // remove individual tag from tag list
+  const handleRemoveTag = (tagId) => {
+    console.warn('remove tag: ', tagId);
+  };
+
   return (
     <View style={styles.container}>
-      <Icon name="tag" size={18} color={theme.accentColor} style={{ padding: 6 }} />
-      {/* rendering tag list here */}
+      <Icon name="tag" size={18} color={theme.textSecondary} style={{ padding: 6 }} />
+
       <View style={styles.tagsFlex}>
-        {tagList.map((tag) => {
-          return (
-            <View style={styles.tagSingle} key={tag.id}>
-              <Text style={styles.tagSingleText}>{tag.name}</Text>
-              <TouchableOpacity>
-                <Icon style={styles.tagSingleRemove} name="minus-circle" size={16} color={theme.accentColor} />
-              </TouchableOpacity>
-            </View>
-          );
-        })}
+        {tagList.length > 0
+          ? // rendering tag list here
+            tagList.map((tag) => {
+              return (
+                <View style={styles.tagSingle} key={tag.id}>
+                  <Text style={styles.tagSingleText}>{tag.name}</Text>
+                  <TouchableOpacity onPress={() => handleRemoveTag(tag.id)}>
+                    <Icon style={styles.tagSingleRemove} name="minus-circle" size={16} color={theme.accentColor} />
+                  </TouchableOpacity>
+                </View>
+              );
+            })
+          : null}
+        {/* tag add text input */}
+        <View>
+          <TextInput
+            style={styles.tagAddInput}
+            value={tagText}
+            onChangeText={(text) => handleTagInputChange(text)}
+            placeholder="Add Tag..."
+            onSubmitEditing={() => handleTagSubmit()}
+            // after submit prevent focus lose
+            blurOnSubmit={false}
+          />
+        </View>
       </View>
-      {/* tag add text input */}
-      {/* <View>
-        <TextInput style={{ borderWidth: 1 }} />
-      </View> */}
     </View>
   );
 };
@@ -55,12 +84,20 @@ const styles = StyleSheet.create({
     marginBottom: 5
   },
   tagSingleText: {
-    fontSize: 12,
-    lineHeight: 14
+    fontSize: 14,
+    lineHeight: 16
   },
   tagSingleRemove: {
     marginRight: -8,
     padding: 4
+  },
+  tagAddInput: {
+    padding: 0,
+    borderWidth: 0,
+    fontSize: 14,
+    marginLeft: 10,
+    minWidth: 80,
+    color: theme.textMain
   }
 });
 
