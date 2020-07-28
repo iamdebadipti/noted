0628ceafe9a3e845d414eee6cbd162eec7b30ad2
iamdebadipti <iamdebadipti@gmail.com>
2020-07-28 16:54:02 +0530

    added color

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ export const theme = {
   textSecondary: '#888888',
   activeOpacity: 0.6,
   lightGray: '#dedede',
+  lightestGray: '#efefef',
   headingFontSize: 20,
   // common components
   buttonStyle: {
