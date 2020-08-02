cd5ba1c16b6d37a64afe1e38ddd606e75174a420
iamdebadipti <iamdebadipti@gmail.com>
2020-08-02 14:17:56 +0530

    added sign out button

--- components/DrawerComponent.js
+++ components/DrawerComponent.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { Button } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const DrawerComponent = ({ ...props }) => {
+  // handle sign out
+  const handleSignOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
+      <Button title="Sign Out" onPress={() => handleSignOut()} />
     </DrawerContentScrollView>
   );
 };
